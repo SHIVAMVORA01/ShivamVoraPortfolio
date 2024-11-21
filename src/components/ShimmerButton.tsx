2600781aa63f6b333b60db18662a80a78b365cdd
Shivam Vora <shivam.vora@belden.com>
2024-11-21 18:01:53 +0530

    thirthy eight

--- src/components/ShimmerButton.tsx
+++ src/components/ShimmerButton.tsx
@@ -26,7 +26,6 @@ const ShimmerButton = React.forwardRef<HTMLButtonElement, ShimmerButtonProps>(
     },
     ref,
   ) => {
-    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
     return (
       <button
         style={
@@ -40,7 +39,7 @@ const ShimmerButton = React.forwardRef<HTMLButtonElement, ShimmerButtonProps>(
           } as CSSProperties
         }
         className={cn(
-          "group relative z-0 flex cursor-pointer items-center justify-center overflow-hidden whitespace-nowrap border border-white/10 px-6 py-3 text-white [background:var(--bg)] [border-radius:var(--radius)] dark:text-black",
+          "group relative z-0 flex cursor-pointer items-center justify-center overflow-hidden whitespace-nowrap border border-white/10 px-6 py-3 text-white [background:var(--bg)] [border-radius:var(--radius)]",
           "transform-gpu transition-transform duration-300 ease-in-out active:translate-y-px",
           className,
         )}
@@ -64,7 +63,7 @@ const ShimmerButton = React.forwardRef<HTMLButtonElement, ShimmerButtonProps>(
           style={{
             background: "white",
             WebkitBackgroundClip: "text",
-            WebkitTextFillColor: isDarkMode ? "black" : "transparent",
+            WebkitTextFillColor: "transparent",
           }}
         >
           {children}
